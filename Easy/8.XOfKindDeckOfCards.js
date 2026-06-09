b4512eea6b8abda870be162fcc0a9cdb21ce10f7
NiushaEbrahimi <n.ebrahiminoor@gmail.com>
2026-06-09 18:13:53 +0330

    tried 2 other problems

---
+++ Easy/8.XOfKindDeckOfCards.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} deck
+ * @return {boolean}
+ */
+
+// try again
+
+var hasGroupsSizeX = function(deck) {
+    const freq = new Map();
+
+    for (const card of deck) {
+        freq.set(card, (freq.get(card) || 0) + 1);
+    }
+
+    let result = 0;
+
+    for (const count of freq.values()) {
+        result = gcd(result, count);
+    }
+
+    return result > 1;
+};
+
+function gcd(a, b) {
+    console.log(`a: ${a} b: ${b} a%b ${(a%b)}`)
+    while (b !== 0) {
+        [a, b] = [b, a % b];
+    }
+    console.log(`a: ${a} b: ${b} a%b ${(a%b)}`)
+    return a;
+}
+hasGroupsSizeX([1,1,1,1,2,2,2,2,2])
