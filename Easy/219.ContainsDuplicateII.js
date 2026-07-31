bb524d98df3ef6dc39e8172162cb2e9b45f1950f
NiushaEbrahimi <n.ebrahiminoor@gmail.com>
2026-07-31 17:01:37 +0330

    Contains Duplicate ||

---
+++ Easy/219.ContainsDuplicateII.js
@@ -0,0 +1,53 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+
+// TODO: edge case, nums = [99,99] k =2 (try again) IMP
+
+var containsNearbyDuplicate = function(nums, k) {
+    let i = 0;
+    while( i + k < nums.length ){
+        let left = i;
+        let right = i + k;
+        const newSet = new Set();
+        while(left !== right){
+            if(newSet.has(nums[left]) || newSet.has(nums[right])){
+                return true
+            }
+            newSet.add(nums[left])
+            newSet.add(nums[right])
+            console.log(`left ${left} rihgt ${right}`)
+            left++ 
+            right--
+        }
+        i = i + k + 1
+    }
+    return false
+};
+
+// best answer
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    let set = new Set();
+
+    for (let n = 0; n < nums.length; n++){
+        if (set.has(nums[n])){
+            return true;
+        }
+
+        set.add(nums[n]);
+
+        if (n >= k){
+            set.delete(nums[n - k])
+        }
+    }
+
+    return false;
+};
